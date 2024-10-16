03a393226dc8609b1d9e334745b7d30954354f0a
Domin Kim <doremin98@gmail.com>
2024-10-16 20:22:06 +0900

    [UPDATE] Feature 모듈을 추가 및 모듈 구조 변경

--- Project.swift
+++ Project.swift
@@ -16,37 +16,98 @@ let project = Project(
       sources: ["HotCoffeeHaterCore/Sources/**"],
       resources: ["HotCoffeeHaterCore/Resources/**"]
     ),
-    .target(
-      name: "HotCoffeeHaterCoreTests",
-      destinations: .iOS,
-      product: .unitTests,
-      bundleId: bundleId("HotCoffeeHaterCoreTests"),
-      sources: ["HotCoffeeHaterCore/Tests/**"],
-      dependencies: [
-        .target(name: "HotCoffeeHaterCore")
-      ]
-    ),
     
     // Data Layer
     .target(
-      name: "HotCoffeeHaterModels",
+      name: "StoresFeatureEntity",
       destinations: .iOS,
       product: .framework,
-      bundleId: bundleId("HotCoffeeHaterModels"),
-      sources: ["HotCoffeeHaterModels/Sources/**"]
+      bundleId: bundleId("StoresFeatureEntity"),
+      sources: ["StoresFeatureEntity/Sources/**"]
+    ),
+
+    .target(
+      name: "StoresFeatureRepositoryImplementation",
+      destinations: .iOS,
+      product: .framework,
+      bundleId: bundleId("StoresFeatureRepositoryImplementation"),
+      sources: ["StoresFeatureRepositoryImplementation/Sources/**"],
+      dependencies: [
+        .target(name: "StoresFeatureRepository"),
+        .target(name: "StoresFeatureEntity")
+      ]
+    ),
+    
+    // Domain Layer
+    .target(
+      name: "StoresFeatureRepository",
+      destinations: .iOS,
+      product: .framework,
+      bundleId: bundleId("StoresFeatureRepository"),
+      sources: ["StoresFeatureRepository/Sources/**"]
+    ),
+    
+    // Features
+    // --Stores Feature (SwiftUI)
+    .target(
+      name: "StoresFeature",
+      destinations: .iOS,
+      product: .framework,
+      bundleId: bundleId("StoresFeature"),
+      sources: ["StoresFeature/Sources/**"],
+      dependencies: [
+        .target(name: "StoresFeatureRepository"),
+        .target(name: "StoresFeatureRepositoryImplementation"),
+        .target(name: "HotCoffeeHaterCore")
+      ]
     ),
     .target(
-      name: "HotCoffeeHaterModelsTests",
+      name: "StoresFeatureTests",
       destinations: .iOS,
       product: .unitTests,
-      bundleId: bundleId("HotCoffeeHaterModelsTests"),
-      sources: ["HotCoffeeHaterModels/Tests/**"],
+      bundleId: bundleId("StoresFeatureTests"),
+      sources: ["StoresFeature/Tests/**"],
       dependencies: [
-        .target(name: "HotCoffeeHaterModels")
+        .target(name: "StoresFeature")
+      ]
+    ),
+    .target(
+      name: "StoresFeatureExample",
+      destinations: .iOS,
+      product: .app,
+      bundleId: bundleId("StoresFeatureExample"),
+      sources: ["StoresFeature/Example/**"],
+      dependencies: [
+        .target(name: "StoresFeature")
+      ]
+    ),
+    
+    // --Stores Feature (UIKit)
+    .target(
+      name: "StoresFeatureUIKit",
+      destinations: .iOS,
+      product: .framework,
+      bundleId: bundleId("StoresFeatureUIKit"),
+      sources: ["StoresFeatureUIKit/Sources/**"],
+      dependencies: [
+        .target(name: "StoresFeatureRepository"),
+        .target(name: "StoresFeatureRepositoryImplementation"),
+        .target(name: "HotCoffeeHaterCore")
+      ]
+    ),
+    .target(
+      name: "StoresFeatureUIKitTests",
+      destinations: .iOS,
+      product: .unitTests,
+      bundleId: bundleId("StoresFeatureUIKitTests"),
+      sources: ["StoresFeatureUIKit/Tests/**"],
+      dependencies: [
+        .target(name: "StoresFeatureUIKit")
       ]
     ),
     
     // App
+    // --SwiftUI Version
     .target(
       name: "HotCoffeeHater",
       destinations: .iOS,
@@ -60,7 +121,7 @@ let project = Project(
       sources: ["HotCoffeeHater/Sources/**"],
       resources: ["HotCoffeeHater/Resources/**"],
       dependencies: [
-        .target(name: "HotCoffeeHaterCore")
+        .target(name: "StoresFeature")
       ]
     ),
     .target(
@@ -72,6 +133,24 @@ let project = Project(
       sources: ["HotCoffeeHater/Tests/**"],
       dependencies: [
         .target(name: "HotCoffeeHater")
+      ]
+    ),
+    
+    // --UIKit Version
+    .target(
+      name: "HotCoffeeHaterUIKit",
+      destinations: .iOS,
+      product: .app,
+      bundleId: bundleId("HotCoffeeHaterUIKit"),
+      infoPlist: .extendingDefault(
+        with: [
+          "UILaunchScreen": .dictionary([:])
+        ]
+      ),
+      sources: ["HotCoffeeHaterUIKit/Sources/**"],
+      resources: ["HotCoffeeHaterUIKit/Resources/**"],
+      dependencies: [
+        .target(name: "StoresFeatureUIKit")
       ]
     ),
   ]
